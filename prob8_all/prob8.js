72ca7e7d08a053b3ded0db9f9816bddf45268837
alekulyn <alekulyn@users.noreply.github.com>
2015-01-26 01:00:12 -0600

    ProjectEuler problem 2 in Javascript added.  Final answer/value for problem 8 added.
    Signed-off-by: alekulyn <alekulyn@users.noreply.github.com>

--- prob8_all/prob8.js
+++ prob8_all/prob8.js
@@ -1,5 +1,5 @@
 // The four adjacent digits in the 1000-digit number (below) that have the greatest product are 9 * 9 * 8 * 9 = 5832.
-// Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+// Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?  23514624000
 
 var theNUMBER =
 "73167176531330624919225119674426574742355349194934\
