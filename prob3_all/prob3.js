c263308927d0f0fcf1e1964c293826c0a8e445b5
alekulyn <alekulyn@users.noreply.github.com>
2015-01-26 01:52:27 -0600

    Sanitize problem 3's input.
    Signed-off-by: alekulyn <alekulyn@users.noreply.github.com>

--- prob3_all/prob3.js
+++ prob3_all/prob3.js
@@ -2,6 +2,15 @@
 // What is the largest prime factor of the number 600851475143?  6857
 
 var theNUMBER = parseInt(prompt("Which number do you what to find the largest prime factor of?"));
+
+// Sanitize input
+theNUMBER = Math.floor(theNUMBER);
+if (theNUMBER < 2 || theNUMBER === NaN)
+{
+	alert("Must be an integer and greater than 1.");
+	return;
+}
+
 var prime = 3;
 
 breakDown();
